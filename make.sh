73d0b52235e9f0e55e5abe37a106e49997bef4b7
guixiangyu <guixiangyu1@gmail.com>
2019-10-24 09:31:39 +0800

    init

--- make.sh
+++ make.sh
@@ -1,4 +1,7 @@
 #!/usr/bin/env bash
-g++ init.cpp -o init.so -fPIC -shared -pthread -O3 -march=native
+#g++ init.cpp -o init.so -fPIC -shared -pthread -O3 -march=native
 #   文件     输出  库   独立路径  共享  多线程   优化级别  本地cpu
-g++ test.cpp -o test.so -fPIC -shared -pthread -O3 -march=native
+#g++ test.cpp -o test.so -fPIC -shared -pthread -O3 -march=native
+
+g++ init.cpp -o init.so -fPIC -shared -O3 -march=native
+g++ test.cpp -o test.so -fPIC -shared -O3 -march=native
